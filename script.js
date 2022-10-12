626a5d8496d9e83ed3da7a9d475dc2ad7b372520
kovasah <samkovacevic05@gmail.com>
2022-10-12 15:59:39 +0200

    calculate feature

---
+++ script.js
@@ -0,0 +1,37 @@
+let btns = Array.from(document.querySelectorAll('button'));
+console.log(btns);
+let display = document.querySelector(".results");
+
+
+// function display() {
+//     btns.forEach(button => {
+//         content.textContent = 'hello'
+//     });
+// }
+
+// btns.addEventListener("click", display);
+
+btns.map( button => {
+    button.addEventListener('click', (e) => {
+        console.log(e.target.innerText);
+        switch(e.target.innerText){
+            case 'AC':
+                display.innerText = '0';
+                break;
+            case '=':
+                try{
+                    display.innerText = eval(display.innerText);
+                } catch {
+                    display.innerText = "Error"
+                }
+                break;
+            case 'del':
+                if (display.innerText){
+                   display.innerText = display.innerText.slice(0, -1);
+                }
+                break;
+            default:
+                display.innerText += e.target.innerText;
+        }
+    });
+});
